abd9ef529c1435f045fc318650d131aa4ebbed30
Juan Antonio Mugica <138845558+Juan-An-ML@users.noreply.github.com>
2024-08-18 17:27:12 -0600

    #

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -1,1 +1,13 @@
 # The central code for PAP-II ! =) 
+# EDA FOR ECONOMIC INDICATORS. 
+# import libraries.....
+
+import pandas as pd
+
+# Load the Excel file
+file_path = '/Users/juan.mugica/Desktop/PAP-II./PAP-ERS/Data/economic_indicators_data.xlsx'
+excel_data = pd.ExcelFile(file_path)
+
+# Display sheet names to understand the structure
+sheet_names = excel_data.sheet_names
+sheet_names
